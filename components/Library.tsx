ed810c0c506a0e010ef8930e46d69cde1c6cc8d0
Opeyemi Akande <oakande@parthianpartnersng.com>
2023-07-11 10:00:37 +0100

    List Library items on Sidebar

--- components/Library.tsx
+++ components/Library.tsx
@@ -5,11 +5,19 @@ import { AiOutlinePlus } from "react-icons/ai";
 import useAuthModal from "@/hooks/useAuthModal";
 import { useUser } from "@/hooks/useUser";
 import useUploadModal from "@/hooks/useUploadModal";
+import { Song } from "@/types";
+import useOnPlay from "@/hooks/useOnPlay";
+import MediaItem from "@/components/MediaItem";
+interface LibraryProps {
+  songs: Song[];
+}
 
-const Library = () => {
+const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
+
+  const onPlay = useOnPlay(songs);
 
   const onClick = () => {
     if (!user) {
@@ -34,7 +42,15 @@ const Library = () => {
           className="text-neutral-400 cursor-pointer hover:text-white transition"
         />
       </div>
-      <div className="flex flex-col gap-y-2 mt-4 px-3">List of Songs!</div>
+      <div className="flex flex-col gap-y-2 mt-4 px-3">
+        {songs.map((item) => (
+          <MediaItem
+            onClick={(id: string) => onPlay(id)}
+            key={item.id}
+            data={item}
+          />
+        ))}
+      </div>
     </div>
   );
 };
